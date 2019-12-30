0c82514067b9ecaedcdbd31312ead38cb691e792
Andrew Trigg <andrewt51@hotmail.com>
2019-12-30 11:29:39 +0000

    Change styling of main component

--- src/layout/main-area.js
+++ src/layout/main-area.js
@@ -1,11 +1,5 @@
 import styled from 'styled-components';
 
 export const MainArea = styled.main`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  overflow: auto;
   margin-top: ${props => props.theme.navbar.height};
 `;
